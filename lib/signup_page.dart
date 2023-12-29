abdfd13e8fe4a10c02adda477ae0046d686cf7b4
prantiq <u1904103@student.cuet.ac.bd>
2023-12-29 20:53:02 +0600

    prantiq

---
+++ lib/signup_page.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+
+import 'login_page.dart';
+
+class SignupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 400,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/background.png'),
+                          fit: BoxFit.fill
+                      )
+                  ),
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                        left: 30,
+                        width: 80,
+                        height: 200,
+                        child: FadeInUp(duration: Duration(seconds: 1), child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/light-1.png')
+                              )
+                          ),
+                        )),
+                      ),
+                      Positioned(
+                        left: 140,
+                        width: 80,
+                        height: 150,
+                        child: FadeInUp(duration: Duration(milliseconds: 1200), child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/light-2.png')
+                              )
+                          ),
+                        )),
+                      ),
+                      Positioned(
+                        right: 40,
+                        top: 40,
+                        width: 80,
+                        height: 150,
+                        child: FadeInUp(duration: Duration(milliseconds: 1300), child: Container(
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage('assets/images/clock.png')
+                              )
+                          ),
+                        )),
+                      ),
+                      Positioned(
+                        child: FadeInUp(duration: Duration(milliseconds: 1600), child: Container(
+                          margin: EdgeInsets.only(top: 50),
+                          child: Center(
+                            child: Text("Signup", style: TextStyle(color: Colors.white, fontSize: 40, fontWeight: FontWeight.bold),),
+                          ),
+                        )),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0, left: 30.0, right: 30.0, bottom: 30.0),
+                  child: Column(
+                    children: <Widget>[
+                      FadeInUp(duration: Duration(milliseconds: 1800), child: Container(
+                        padding: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Color.fromRGBO(143, 148, 251, 1)),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Color.fromRGBO(143, 148, 251, .2),
+                                  blurRadius: 20.0,
+                                  offset: Offset(0, 10)
+                              )
+                            ]
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color:  Color.fromRGBO(143, 148, 251, 1)))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Name",
+                                    hintStyle: TextStyle(color: Colors.grey[700])
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              decoration: BoxDecoration(
+                                  border: Border(bottom: BorderSide(color:  Color.fromRGBO(143, 148, 251, 1)))
+                              ),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Email Address",
+                                    hintStyle: TextStyle(color: Colors.grey[700])
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(8.0),
+                              child: TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(color: Colors.grey[700])
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      )),
+                      SizedBox(height: 30,),
+                      FadeInUp(duration: Duration(milliseconds: 1900), child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            gradient: LinearGradient(
+                                colors: [
+                                  Color.fromRGBO(143, 148, 251, 1),
+                                  Color.fromRGBO(143, 148, 251, .6),
+                                ]
+                            )
+                        ),
+                        child: Center(
+                          child: Text("Signup", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                        ),
+                      )),
+                      SizedBox(height: 30,),
+                      GestureDetector(
+                        onTap: () {
+                          // Implement your "Forgot Password?" action
+                          print('login is tapped');
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => LoginPage()),
+                          );
+                        },
+                        child: FadeInUp(duration: Duration(milliseconds: 2000), child: Text("Login!", style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),)),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
